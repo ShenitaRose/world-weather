ad385e084c75dc90fb5fd8361b5951f03ccfc8cc
ShenitaRose <simsationelle@gmail.com>
2022-07-14 11:52:44 +0100

    Added the github repo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by {""}
+        <a href='#' target="_blank">Shenita Rose </a> and is {""}
+        <a href="https://github.com/ShenitaRose/world-weather" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
